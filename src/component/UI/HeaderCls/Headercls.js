818ea8fdf4fbc1da73c705309046ae96540206d0
vladimirovsk <vladimirovsk@gmail.com>
2020-08-12 20:11:36 +0300

    pc

--- src/component/UI/HeaderCls/Headercls.js
+++ src/component/UI/HeaderCls/Headercls.js
@@ -4,7 +4,6 @@ import { withStyles, useTheme } from "@material-ui/core/styles";
 import {makeStyles} from '@material-ui/styles';
 import {useMediaQuery, AppBar, useScrollTrigger, Toolbar, Button, Link} from '@material-ui/core';
 import logo from '../../../assets/logo.png'; 
-import Theme from "../Theme";
 
 
 import {
@@ -12,7 +11,6 @@ import {
     //getLanguage,
     translate,
   } from 'react-switch-lang';
-
 
 
   class Headerscls extends Component {
@@ -25,8 +23,6 @@ import {
 		anchorEl:null,
 		classes:this.thStyles(),
 		open:false,
-
-
       }
 	}
 
@@ -87,15 +83,6 @@ import {
 		}
 	  }));
 	
-	componentDidMount() {
-		this.setState({theme:useTheme});
-		//console.log(thStyles);
-		this.setState({classes: this.thStyles()});
-		this.setState({open: false})
-		this.setState({matches: useMediaQuery(this.theme.breakpoints.down("md"))})
-		//this.props.classes = useTheme();
-    }
-
 
       render () {
 		const {t} = this.props;
@@ -137,64 +124,16 @@ import {
 
   }
 
-  const thStyles = makeStyles(theme => ({
-	toolbarMargin:{
-	  ...theme.mixins.toolbar,
-	  marginBottom:"3em",
-	},
-  
-	logo:{
-	height:"4em",
-	[theme.breakpoints.down("md")]:{
-	height:"3em"  },
-	[theme.breakpoints.down("sm")]:{
-	  height:"2em"  }
-	},
-  
-	logoContainer:{
-  
-	  padding:'5px',
-	  "&:hover":{
-		backgroundColor:"transparent"
-	  }
-	},
-	
-	tabContainer:{
-	marginLeft:'auto',
-	},
-   
-	tab:{
-	...theme.typography.tab,
-	minWidth:10,
-	  marginLeft:'25px',
-	 },
-  
-	 button:{
-	   ...theme.typography.signin,
-		height:"45px",
-	 },
-   
-	 menu:{
-	   backgroundColor: theme.palette.common.arcBlue,
-	 color  :"white",
-	 borderRadius:'0px'
-	 },
-	 menuItem:{
-	...theme.typography.tab,
-	opacity:0.7,
-	"&:hover": {
-	  opacity:1,
-	  color:"red"
-  
-	},
-	
-	selected: { /* Increase the specificity */
-	  color: "green"
-	 }
-	}
-  }));
+  componentDidMount() {
+	this.setState({theme:useTheme});
+	//console.log(thStyles);
+	this.setState({classes: this.thStyles()});
+	this.setState({open: false})
+	this.setState({matches: useMediaQuery(this.theme.breakpoints.down("md"))})
+	//this.props.classes = useTheme();
+} 
 
- 
+
 function mapStateToProps(state){
   //console.log(new Date(localStorage.getItem('expirationDate')))
   //console.log(new Date(state.auth.expData))
